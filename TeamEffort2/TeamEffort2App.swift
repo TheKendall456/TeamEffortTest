775ea87a6ef79185a0de01dc1012a86d36d0d1bd
TheKendall456 <116093767+TheKendall456@users.noreply.github.com>
2024-04-10 09:35:14 -0500

    Initial Commit

---
+++ TeamEffort2/TeamEffort2App.swift
@@ -0,0 +1,17 @@
+//
+//  TeamEffort2App.swift
+//  TeamEffort2
+//
+//  Created by Kendall Goethals on 4/10/24.
+//
+
+import SwiftUI
+
+@main
+struct TeamEffort2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
