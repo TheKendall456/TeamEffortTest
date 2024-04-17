c73d0308f4206a35c865b577189bc0eb365abed6
Kendall <goethalskendall@gmail.com>
2024-04-17 08:45:35 -0500

    GetRequest

--- TeamEffort2/MovieData.swift
+++ TeamEffort2/MovieData.swift
@@ -1,8 +1,30 @@
-//
-//  MovieData.swift
-//  TeamEffort2
-//
-//  Created by Kendall Goethals on 4/15/24.
-//
-
-import Foundation
+   
+    import Foundation
+     
+    var globalData: Data?
+     
+    func fetchData() async throws {
+        let url = URL(string: "https://api.themoviedb.org/3/authentication")!
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.timeoutInterval = 10
+        request.allHTTPHeaderFields = [
+            "accept": "application/json",
+            "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlNjQ5ODg1YTBmYWI2NThkZDNlMTI0MmY3NWE4ODExOSIsInN1YiI6IjY1Mzk5NDZiMjgxMWExMDE0ZDYwYWZiMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.dT7HyNSXcyBaeud8FN0XzNCmNpAWXFCzJ7WmrGGJT9A"
+        ]
+     
+        let (data, response) = try await URLSession.shared.data(for: request)
+        globalData = data // Store the fetched data in the global variable
+        print(String(decoding: data, as: UTF8.self))
+    }
+     
+    @main // Entry point of the program
+    struct Main {
+        static func main() async {
+            do {
+                try await fetchData()
+            } catch {
+                print("Error: \(error)")
+            }
+        }
+    }
