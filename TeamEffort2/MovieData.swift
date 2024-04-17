2c5f29d2cfd114641bd33e0f216e6bdd1097c45f
Kendall <goethalskendall@gmail.com>
2024-04-17 08:47:18 -0500

    GetRequest

--- TeamEffort2/MovieData.swift
+++ TeamEffort2/MovieData.swift
@@ -13,7 +13,7 @@
             "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlNjQ5ODg1YTBmYWI2NThkZDNlMTI0MmY3NWE4ODExOSIsInN1YiI6IjY1Mzk5NDZiMjgxMWExMDE0ZDYwYWZiMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.dT7HyNSXcyBaeud8FN0XzNCmNpAWXFCzJ7WmrGGJT9A"
         ]
      
-        let (data, response) = try await URLSession.shared.data(for: request)
+        let (data, _) = try await URLSession.shared.data(for: request)
         globalData = data // Store the fetched data in the global variable
         print(String(decoding: data, as: UTF8.self))
     }
