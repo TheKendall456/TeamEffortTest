c26900fa30e545bb598b7cdc8c60ef37d2c44c33
Kendall <goethalskendall@gmail.com>
2024-04-24 09:30:26 -0500

    UpdateNewViews

---
+++ TeamEffort2/RandomView.swift
@@ -0,0 +1,18 @@
+//
+//  RandomView.swift
+//  TeamEffort2
+//
+//  Created by GOETHALS, KENDALL A. on 4/24/24.
+//
+
+import SwiftUI
+
+struct RandomView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RandomView()
+}
